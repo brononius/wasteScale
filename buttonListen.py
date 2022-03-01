bf4f5c01dc8c77b0d5166cd2e81e743e23a2ef4c
brononius <ben@oniria.be>
2022-03-01 18:22:56 +0100

    Update buttonListen.py

--- buttonListen.py
+++ buttonListen.py
@@ -3,27 +3,33 @@ import requests
 import time
 import RPi.GPIO as GPIO
 
-#Buttons 5 6 12 24 25 26 
+scriptPath="/diy/wasteScale"		#Path where the scripts are installed
+
+gpio_pinA=5                         # The GPIO pin your the button is attached to
+pinWasteA="Rest"                    # Corresponding name (waste) for that button
+
+gpio_pinB=6                         # The GPIO pin the button is attached to
+pinWasteB="PMD"                     # Corresponding name (waste) for that button
+
+gpio_pinC=12                        # The GPIO pin the button is attached to
+pinWasteC="Papier"                  # Corresponding name (waste) for that button
+
+gpio_pinD=24                        # The GPIO pin the button is attached to
+pinWasteD="Glas"                    # Corresponding name (waste) for that button
+
+gpio_pinE=25                        # The GPIO pin the button is attached to
+pinWasteE="GFT"                     # Corresponding name (waste) for that button
+
+gpio_pinF=26                        # The GPIO pin the button is attached to
+pinWasteF="Varia"                   # Corresponding name (waste) for that button
 
 
-gpio_pinA=5     # The GPIO pin the button is attached to
-pinWasteA="Rest"
-cmdWasteA= "/usr/bin/bash /diy/wasteScale/currentWeight.sh "+ pinWasteA
-gpio_pinB=6     # The GPIO pin the button is attached to
-pinWasteB="PMD"
-cmdWasteB= "/usr/bin/bash /diy/wasteScale/currentWeight.sh "+ pinWasteB
-gpio_pinC=12     # The GPIO pin the button is attached to
-pinWasteC="Papier"
-cmdWasteC= "/usr/bin/bash /diy/wasteScale/currentWeight.sh "+ pinWasteC
-gpio_pinD=24     # The GPIO pin the button is attached to
-pinWasteD="Glas"
-cmdWasteD= "/usr/bin/bash /diy/wasteScale/currentWeight.sh "+ pinWasteD
-gpio_pinE=25     # The GPIO pin the button is attached to
-pinWasteE="GFT"
-cmdWasteE= "/usr/bin/bash /diy/wasteScale/currentWeight.sh "+ pinWasteE
-gpio_pinF=26     # The GPIO pin the button is attached to
-pinWasteF="Varia"
-cmdWasteF= "/usr/bin/bash /diy/wasteScale/currentWeight.sh "+ pinWasteF
+cmdWasteA= "/usr/bin/bash "+ scriptPath +"/currentWeight.sh "+ pinWasteA
+cmdWasteB= "/usr/bin/bash "+ scriptPath +"/currentWeight.sh "+ pinWasteB
+cmdWasteC= "/usr/bin/bash "+ scriptPath +"/currentWeight.sh "+ pinWasteC
+cmdWasteD= "/usr/bin/bash "+ scriptPath +"/currentWeight.sh "+ pinWasteD
+cmdWasteE= "/usr/bin/bash "+ scriptPath +"/currentWeight.sh "+ pinWasteE
+cmdWasteF= "/usr/bin/bash "+ scriptPath +"/currentWeight.sh "+ pinWasteF
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(gpio_pinA, GPIO.IN, pull_up_down=GPIO.PUD_UP)
@@ -47,5 +53,4 @@ while True:
         os.system(cmdWasteE)
     if GPIO.input(gpio_pinF) == False: # Listen for the press, the loop until it steps
         os.system(cmdWasteF)
-
 
